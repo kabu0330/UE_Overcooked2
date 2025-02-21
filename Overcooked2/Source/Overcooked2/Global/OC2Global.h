4fd161d868b564db7d0ed1c928769860bd537ef1
yongilkim01 <yongil.k01@gmail.com>
2025-02-21 11:14:22 +0900

    김용일 : 글로벌 함수, 상수 클래스 추가

---
+++ Overcooked2/Source/Overcooked2/Global/OC2Global.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OC2Global.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCOOKED2_API UOC2Global : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable)
+	static TArray<FAssetData> GetAssetPackageName(UClass* Class, const FString& AssetName, FString& Path);
+
+	UFUNCTION(BlueprintCallable)
+	static TArray<FAssetData> GetAssetDataArray(UClass* Class);
+	
+};
