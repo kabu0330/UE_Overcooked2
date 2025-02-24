780db9dbee0e420568d8568f0a66ed71c4bc9611
yongilkim01 <yongil.k01@gmail.com>
2025-02-24 17:04:12 +0900

    김용일 : 음식 데이터 테이블, 주문 구조체 추가

---
+++ Overcooked2/Source/Overcooked2/Global/Component/OrderManageComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Global/Data/FoodDataTable.h"
+#include "OrderManageComponent.generated.h"
+
+USTRUCT()
+struct OVERCOOKED2_API FOrder
+{
+	GENERATED_BODY()
+
+	std::vector<FFoodDataRow> RequiredFoods;
+};
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class OVERCOOKED2_API UOrderManageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOrderManageComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+		
+};
