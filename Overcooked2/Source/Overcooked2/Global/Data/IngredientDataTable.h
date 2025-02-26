39674be0cfd4c416cb270e39a4dd28d821d0f5ab
yongilkim01 <yongil.k01@gmail.com>
2025-02-26 17:17:56 +0900

    김용일 : 재료 데이터 추가

--- Overcooked2/Source/Overcooked2/Global/Data/IngredientDataTable.h
+++ Overcooked2/Source/Overcooked2/Global/Data/IngredientDataTable.h
@@ -7,6 +7,7 @@
 #include "Global/OC2Enum.h"
 #include "IngredientDataTable.generated.h"
 
+// 겹침 손질이 존재하는 데이터 속성
 USTRUCT(BlueprintType)
 struct FIngredientCookDataRow 
 {
@@ -23,6 +24,7 @@ public:
 	UStaticMesh* CookMesh = nullptr;
 };
 
+// 일반 재료 데이터 열
 USTRUCT(BlueprintType)
 struct FIngredientDataRow : public FTableRowBase
 {
@@ -38,9 +40,10 @@ struct FIngredientDataRow : public FTableRowBase
 	UStaticMesh* BaseMesh = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
-	TArray<FIngredientCookDataRow> Rows;
+	TArray<FIngredientCookDataRow> StateRows;
 };
 
+// 손질 가능한 요리 속성
 USTRUCT(BlueprintType)
 struct FCookableIngredient 
 {
@@ -55,6 +58,21 @@ public:
 
 };
 
+// 재료 조합별 메쉬 데이터 속성
+USTRUCT(BlueprintType)
+struct FRecipeMeshDataRow
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+	TArray<FCookableIngredient> Array;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+	UStaticMesh* BaseMesh = nullptr;
+};
+
+// 레시피 데이터 열
 USTRUCT(BlueprintType)
 struct FRecipeDataRow : public FTableRowBase
 {
@@ -66,7 +84,43 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
 	TArray<FCookableIngredient> CookableIngredients;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+	TArray<FRecipeMeshDataRow> CheckMesh;
 };
+
+// 스테이지에서 등장하는 레시피 데이터 열
+USTRUCT(BlueprintType)
+struct FStageCookingDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+	//TArray<FName> StageRecipeNames;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+	// 오징어
+	// 오이
+	// 김
+	// 쌀
+	
+	// 상자
+
+	TArray<EIngredientType> IngredientType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+
+	TArray<FName> AllRecipe;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Global|Data")
+
+	TArray<int> SelectRecipe;
+
+	// void Init();
+
+};
+
 
 /**
  * 
--- Overcooked2/Source/Overcooked2/Global/OC2Enum.h
+++ Overcooked2/Source/Overcooked2/Global/OC2Enum.h
@@ -6,33 +6,43 @@
 #include "UObject/NoExportTypes.h"
 #include "OC2Enum.generated.h"
 
-UENUM(BlueprintType)
-enum class EIngredientState : uint8
-{
-	EIS_NONE      		UMETA(DisplayName = "None"),
-	EIS_CHOPPABLE		UMETA(DisplayName = "채썰기"),
-	EIS_GRILLABLE    	UMETA(DisplayName = "굽기"),
-	EIS_FRYABLE     	UMETA(DisplayName = "튀기기"),
-	EIS_BOILABLE     	UMETA(DisplayName = "끓이기"),
-	EIS_OVERCOOKED      UMETA(DisplayName = "태움"),
-	EIS_MAX				UMETA(DisplayName = "Max"),
-};
 
 UENUM(BlueprintType)
 enum class EIngredientType : uint8
 {
 	EIT_NONE			UMETA(DisplayName = "None"),
-	EIT_FISH			UMETA(DisplayName = "생성"),
+	EIT_FISH			UMETA(DisplayName = "생선"),
 	EIT_CABBAGE			UMETA(DisplayName = "양배추"),
+	EIT_PRAWN			UMETA(DisplayName = "새우"),
 	EIT_TOMATO			UMETA(DisplayName = "토마토"),
+	EIT_LAVER			UMETA(DisplayName = "김"),
+	EIT_RICE			UMETA(DisplayName = "쌀"),
+	EIT_CUCUMBER		UMETA(DisplayName = "오이"),
+};
+
+UENUM(BlueprintType)
+enum class EIngredientState : uint8
+{
+	EIS_NONE      		UMETA(DisplayName = "None"),
+	EIS_CHOPPABLE		UMETA(DisplayName = "손질 전"),
+	EIS_CHOPPED   		UMETA(DisplayName = "손질 후"),
+	EIS_GRILLABLE    	UMETA(DisplayName = "굽기 전"),
+	EIS_GRILLED      	UMETA(DisplayName = "구운 후"),
+	EIS_FRYABLE     	UMETA(DisplayName = "튀기기 전"),
+	EIS_FRYABED     	UMETA(DisplayName = "튀기기 후"),
+	EIS_BOILABLE     	UMETA(DisplayName = "끓이기 전"),
+	EIS_BOILED      	UMETA(DisplayName = "끓이기 후"),
+	EIS_FINISHED     	UMETA(DisplayName = "완제품"),
+	EIS_OVERCOOKED      UMETA(DisplayName = "태움"),
+	EIS_MAX				UMETA(DisplayName = "Max"),
 };
 
 UENUM(BlueprintType)
 enum class ECookingType : uint8
 {
 	ECT_NONE			UMETA(DisplayName = "None"),
-	ECT_INGREDIENT		UMETA(DisplayName = "Ingredient"),
-	ECT_PLATE			UMETA(DisplayName = "Plate"),
+	ECT_INGREDIENT		UMETA(DisplayName = "재료"),
+	ECT_PLATE			UMETA(DisplayName = "요리"),
 };
 
 /**
