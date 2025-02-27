12c468c0b499a658076fe1439e06d9a928f6ffa4
민정 강 <rkddkwl3414@naver.com>
2025-02-27 18:05:26 +0900

    강민정 : UIDevLevel 추가 및 주문 UI 1차 구현

---
+++ Overcooked2/Source/Overcooked2/UI/Cooking/CookingHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CookingHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCOOKED2_API ACookingHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
