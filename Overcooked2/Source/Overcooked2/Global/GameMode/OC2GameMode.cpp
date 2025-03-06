d57a9f5e88d9b0495fe52193019ef713a89b990f
yongilkim01 <yongil.k01@gmail.com>
2025-03-06 11:24:56 +0900

    오류 해결 커밋

---
+++ Overcooked2/Source/Overcooked2/Global/GameMode/OC2GameMode.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/GameMode/OC2GameMode.h"
+
---
+++ Overcooked2/Source/Overcooked2/Global/GameMode/OC2GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "OC2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCOOKED2_API AOC2GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
