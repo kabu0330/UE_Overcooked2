fa0a672b42c890d6d271fb431fee1c4c5d4cf932
yongilkim01 <yongil.k01@gmail.com>
2025-03-13 17:35:21 +0900

    김용일 : 주문 매니저 컴포넌트 클래스 주문 생성 메소드 구현

--- Overcooked2/Source/Overcooked2/UI/Cooking/CookingHUD.cpp
+++ Overcooked2/Source/Overcooked2/UI/Cooking/CookingHUD.cpp
@@ -2,4 +2,19 @@
 
 
 #include "UI/Cooking/CookingHUD.h"
+#include "UI/CookingWidget.h"
+#include "Overcooked2.h"
 
+void ACookingHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CookWidget = Cast<UCookingWidget>(CreateWidget(GetWorld(), CookWidgetObject));
+
+	if (CookWidget == nullptr)
+	{
+		UE_LOG(OVERCOOKED_LOG, Error, TEXT("ÄîÀ§Á¬ÀÌ nullptr ÀÔ´Ï´Ù!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"));
+	}
+
+	CookWidget->AddToViewport();
+}
