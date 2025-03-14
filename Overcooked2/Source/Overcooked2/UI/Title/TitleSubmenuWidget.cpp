56253030495a037a51d80409112b6d401e780259
민정 강 <rkddkwl3414@naver.com>
2025-03-14 18:27:55 +0900

    강민정 : Title Menu 2차 구현

--- Overcooked2/Source/Overcooked2/UI/Title/TitleSubmenuWidget.cpp
+++ Overcooked2/Source/Overcooked2/UI/Title/TitleSubmenuWidget.cpp
@@ -2,4 +2,33 @@
 
 
 #include "UI/Title/TitleSubmenuWidget.h"
+#include "Components/CanvasPanel.h"
+#include "Components/Button.h"
 
+void UTitleSubmenuWidget::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+    IP_Panel->SetRenderTranslation({ 0.0f, -500.0f });
+    IP_Panel->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+    GetWorld()->GetTimerManager().SetTimer(MoveTimerHandle, this, &UTitleSubmenuWidget::UpdateBannerPosition, 0.01f, true);
+}
+
+void UTitleSubmenuWidget::UpdateBannerPosition()
+{     
+
+    if (IP_Panel->GetRenderTransform().Translation.Y >= 0)
+    {
+        GetWorld()->GetTimerManager().ClearTimer(MoveTimerHandle);
+    }
+    MoveYOffset = IP_Panel->GetRenderTransform().Translation.Y;
+    MoveYOffset += 30.0f;
+    IP_Panel->SetRenderTranslation({ 0.0f, MoveYOffset });
+}
+
+
+void UTitleSubmenuWidget::MoveBanner()
+{
+    IP_Panel->SetRenderTranslation({ 0.0f, -500.0f });
+    IP_Panel->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+    GetWorld()->GetTimerManager().SetTimer(MoveTimerHandle, this, &UTitleSubmenuWidget::UpdateBannerPosition, 0.01f, true);
+}
