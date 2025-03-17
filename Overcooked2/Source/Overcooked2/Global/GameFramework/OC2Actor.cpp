b85b4ef8e58bc8d777a018857b8a628a359bd413
성민 류 <baekhun0330@naver.com>
2025-03-17 21:20:25 +0900

    류성민 : OC2Actor 테스트 코드 삭제

--- Overcooked2/Source/Overcooked2/Global/GameFramework/OC2Actor.cpp
+++ Overcooked2/Source/Overcooked2/Global/GameFramework/OC2Actor.cpp
@@ -50,7 +50,7 @@ void AOC2Actor::ApplyMaterialHighlight()
 	}
 
 	DiffuseColorMapWeights.SetNumZeroed(10);
-	float HighlightValue = 100.0f;
+	float HighlightValue = 5.0f;
 	int Count = 0;
 
 	for (UMeshComponent* Mesh : MeshComponents)
@@ -71,9 +71,6 @@ void AOC2Actor::ApplyMaterialHighlight()
 					Mesh->GetMaterials()[i]->GetScalarParameterValue(FName("DiffuseAdd"), DiffuseColorMapWeights[Count]);
 					ExistingDynamicMaterial->SetScalarParameterValue(FName("DiffuseAdd"), HighlightValue);
 					Mesh->SetMaterial(i, ExistingDynamicMaterial);
-					float Result = 0.0f;
-					Mesh->GetMaterials()[i]->GetScalarParameterValue(FName("DiffuseAdd"), Result);
-					Result;
 					++Count;
 					continue;
 				}
@@ -95,40 +92,6 @@ void AOC2Actor::ApplyMaterialHighlight()
 		}
 	}
 
-	//for (int i = 0; i < Mesh->GetNumMaterials(); i++)
-	//{
-	//	if (Mesh->GetMaterials().IsValidIndex(i) && Mesh->GetMaterials()[i] != nullptr)
-	//	{
-	//		UMaterialInstanceDynamic* ExistingDynamicMaterial = Cast<UMaterialInstanceDynamic>(Mesh->GetMaterials()[i]);
-	//		if (ExistingDynamicMaterial)
-	//		{
-	//			// 이미 존재하는 다이나믹 머티리얼이 있으면 값을 변경
-	//			Mesh->GetMaterials()[i]->GetScalarParameterValue(FName("DiffuseColorMapWeight"), DiffuseColorMapWeight);
-	//			ExistingDynamicMaterial->SetScalarParameterValue(FName("DiffuseColorMapWeight"), HighlightValue);
-	//			continue;
-	//		}
-	//	}
-
-	//	// 1. 메시의 머티리얼을 모두 가져온다.
-	//	UMaterialInterface* Material = Mesh->GetMaterial(i);
-	//	if (nullptr != Material)
-	//	{
-	//		// 2. 동적 머티리얼 인스턴스를 생성한다.
-	//		// 기존 머티리얼은 스태틱이라 수정할 수 없다.
-	//		UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(Material, this);
-
-	//		if (nullptr != DynamicMaterial)
-	//		{
-	//			// 3. 기존 머티리얼을 먼저 저장하고
-	//			Material->GetScalarParameterValue(FName("DiffuseColorMapWeight"), DiffuseColorMapWeight);
-	//			//Materials.Add(Material);
-
-	//			// 4. 다이나믹 머티리얼로 교체한다.
-	//			DynamicMaterial->SetScalarParameterValue(FName("DiffuseColorMapWeight"), HighlightValue);
-	//			Mesh->SetMaterial(i, DynamicMaterial);
-	//		}
-	//	}
-	//}
 	bIsHighlighted = true;
 }
 
@@ -136,6 +99,7 @@ void AOC2Actor::RestoreMaterial()
 {
 	TArray<UMeshComponent*> MeshComponents;
 	GetComponents<UMeshComponent>(MeshComponents);
+
 	if (true == MeshComponents.IsEmpty())
 	{
 		return;
