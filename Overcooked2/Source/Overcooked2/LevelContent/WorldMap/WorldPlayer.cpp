7e5992b464b2ef6474f5da1d607f1a9c3e211430
JeonHyeJu <xcpc7@naver.com>
2025-03-20 18:29:30 +0900

    전혜주 : 월드맵 오브젝트 추가중

--- Overcooked2/Source/Overcooked2/LevelContent/WorldMap/WorldPlayer.cpp
+++ Overcooked2/Source/Overcooked2/LevelContent/WorldMap/WorldPlayer.cpp
@@ -69,7 +69,8 @@ void AWorldPlayer::OnMove(const FVector2D& _Vec)
 {
 	if (Controller && HasAuthority())
 	{
-		AddMovementInput(FVector(_Vec.X * 0.5f, _Vec.Y * 0.5f, 0.f));
+		// Use Player's mapping context
+		AddMovementInput(FVector(_Vec.Y * 0.5f, _Vec.X * -0.5f, 0.f));
 		UpdateBusLocation(GetActorLocation());
 	}
 }
