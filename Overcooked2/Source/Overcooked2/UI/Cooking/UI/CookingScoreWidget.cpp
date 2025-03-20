a028b5a72c34d883cf08e07c0a22faf197b24e3b
민정 강 <rkddkwl3414@naver.com>
2025-03-20 14:18:53 +0900

    강민정 : 잘못 제출한 요리 UI 색상 조절

---
+++ Overcooked2/Source/Overcooked2/UI/Cooking/UI/CookingScoreWidget.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Cooking/UI/CookingScoreWidget.h"
+#include "Components/Image.h"
+
+void UCookingScoreWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+}
+
+
