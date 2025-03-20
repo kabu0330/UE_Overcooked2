b3c71f8f18210444a3d371d4eb3ba136220400e5
yongilkim01 <yongil.k01@gmail.com>
2025-03-20 15:21:47 +0900

    주문 서버 동기화 버그 해결

---
+++ Overcooked2/Source/Overcooked2/Global/State/GameState/CookingGameState.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/State/OC2GameState.h"
+
+#include "Global/OC2Const.h"
+#include "Global/OC2Struct.h"
+#include "Global/OC2Enum.h"
+
+#include "CookingGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCOOKED2_API ACookingGameState : public AOC2GameState
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_CreateNewOrder(FOrder Order);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_CompleteOrder(FOrder Order);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_BlinkOrderUI();
+
+private:
+	int FindOrderIndex(FOrder& Order);
+
+private:
+	TArray<FOrder> OrderList;
+	
+};
