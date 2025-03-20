cc39b9bc055addc3f5cbf7ed87b84ef1c0a3a8e3
yongilkim01 <yongil.k01@gmail.com>
2025-03-20 18:37:32 +0900

    충돌 해결 커밋

--- Overcooked2/Source/Overcooked2/Character/OC2Character.cpp
+++ Overcooked2/Source/Overcooked2/Character/OC2Character.cpp
@@ -5,7 +5,6 @@
 #include "EnhancedInputComponent.h"
 #include "OC2CharacterTestTable.h"
 #include "OC2CharacterTestChoppingTable.h"
-#include "LevelContent/Table/ChoppingTable.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Net/UnrealNetwork.h"
 
@@ -132,8 +131,10 @@ void AOC2Character::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 void AOC2Character::SetCharacterName_Implementation(const FString& Name)
 {
 	if (CharacterHeadMap.Contains(Name))
+	{
 		CharacterName = Name;
-	OnRep_ChangeCharacter();
+		OnRep_ChangeCharacter();
+	}
 }
 
 void AOC2Character::OnRep_ChangeCharacter()
@@ -275,9 +276,11 @@ void AOC2Character::Grab_Implementation(ACooking* Cook)
 	GrabbedObject->AttachToChef(this);
 
 	auto DataTable = GrabbedObject->GetIngredientDataTable();
-	
-	GrabbedObject->SetActorRotation(DataTable->Rotation);
-	GrabbedObject->SetActorLocation(GrabComponent->GetComponentLocation());
+	if (DataTable)
+	{
+		GrabbedObject->SetActorRelativeRotation(DataTable->Rotation);
+		GrabbedObject->SetActorRelativeLocation(DataTable->Location);
+	}
 }
 
 void AOC2Character::Drop_Implementation()
@@ -289,10 +292,13 @@ void AOC2Character::Drop_Implementation()
 		// 들고 있는 물체에 대해 상호작용을 실행한다. 바닥에 내려놓는다는 뜻.
 		GrabbedObject->DetachFromChef(this);
 
+		auto DataTable = GrabbedObject->GetIngredientDataTable();
+		if (DataTable)
+		{
+			GrabbedObject->SetActorRotation(DataTable->Rotation);
+		}
+		
 		GrabbedObject->SetActorLocation(GrabComponent->GetComponentLocation());
-		GrabbedObject->SetActorRotation(GetActorRotation());
-
-
 		GrabbedObject = nullptr;
 	}
 }
@@ -312,12 +318,10 @@ void AOC2Character::DoActionPress_Implementation()
 	}
 	else
 	{
-		if (SelectedOC2Actor->IsA(AChoppingTable::StaticClass()))
+		if (SelectedOC2Actor->IsA(AOC2CharacterTestChoppingTable::StaticClass()))
 		{
-			auto Table = Cast<AChoppingTable>(SelectedOC2Actor);
+			auto Table = Cast<AOC2CharacterTestChoppingTable>(SelectedOC2Actor);
 			Chopping(true);
-			Table->ChopIngredient(this);
-			
 		}
 	}
 }
@@ -480,7 +484,7 @@ void AOC2Character::OnOverlapCheck_Implementation(UPrimitiveComponent* Overlappe
 {
 	if (OtherActor && HasAuthority())
 	{
-		//			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, "Hit");
+		//GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, "Hit");
 		AIngredient* Ingredient = Cast<AIngredient>(OtherActor);
 		if (Ingredient) // 캡슐과 충돌한 경우
 		{
@@ -488,7 +492,12 @@ void AOC2Character::OnOverlapCheck_Implementation(UPrimitiveComponent* Overlappe
 			{
 				GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, "Hit");
 				Ingredient->AttachToChef(this);
-				Ingredient->SetActorLocation(GrabComponent->GetComponentLocation());
+				auto DataTable = Ingredient->GetIngredientDataTable();
+				if (DataTable)
+				{
+					GrabbedObject->SetActorRelativeRotation(DataTable->Rotation);
+					GrabbedObject->SetActorRelativeLocation(DataTable->Location);
+				}
 				Ingredient->SetThrowing(false);
 				GrabbedObject = Ingredient;
 				FVector Dir = SweepResult.ImpactPoint - GetActorLocation();
