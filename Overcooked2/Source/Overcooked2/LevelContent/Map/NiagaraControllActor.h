119dc3b433c10a37b2c0b4fb03d0fb85f6795bb1
성민 류 <baekhun0330@naver.com>
2025-04-03 15:53:50 +0900

    류성민 : 스테이지 환경 FX 추가

---
+++ Overcooked2/Source/Overcooked2/LevelContent/Map/NiagaraControllActor.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraComponent.h"
+#include <Global/Component/TimeEventComponent.h>
+#include "NiagaraControllActor.generated.h"
+
+UCLASS()
+class OVERCOOKED2_API ANiagaraControllActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANiagaraControllActor();
+
+	UFUNCTION(BlueprintCallable)
+	void LoopCooldown(float Cooldown, float Duration = 3.0f, float InitDelay = 0.0f)
+	{
+		bUseCycle = true;
+		CycleCooldown = Cooldown;
+		RenderDuration = Duration;
+		InitDelayTime = InitDelay;
+	}
+
+	void InitDelay(float InitDelay = 0.0f)
+	{
+		InitDelayTime = InitDelay;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void SetRelativeOffset(FTransform Transform)
+	{
+		SetActorRelativeLocation(Transform.GetLocation());
+		SetActorRelativeRotation(Transform.GetRotation());
+		SetActorRelativeScale3D(Transform.GetScale3D());
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void SetActivate(bool Value);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void UpdateLoopCycle(float DeltaTime);
+
+private:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UserEdit", meta = (AllowPrivateAccess = "true"))
+	UNiagaraComponent* NiagaraComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UserEdit", meta = (AllowPrivateAccess = "true"))
+	UNiagaraSystem* NiagaraSystem = nullptr;
+
+	UTimeEventComponent* TimeEventComponent = nullptr;
+
+	float CycleCooldown = 0.0f;
+
+	float TimeElpased = 0.0f;
+
+	float RenderDuration = 0.0f;
+	float InitDelayTime = 0.0f;
+
+	bool bUseCycle = false;
+	bool bCanCalled = false;
+	bool bLoopStart = false;
+	bool bIsFirstCalled = false;
+};
