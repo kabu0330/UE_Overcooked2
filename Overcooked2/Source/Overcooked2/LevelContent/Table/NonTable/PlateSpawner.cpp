f57dcc12c3dd62922a5e50074a1731e5d97e2604
dfgh2727 <dfgh2727@naver.com>
2025-04-08 11:49:48 +0900

    이수환 : PlateSpawner 함수 추가중 (수정 전)

--- Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
+++ Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
@@ -21,12 +21,36 @@ void APlateSpawner::Tick(float DeltaTime)
 
 void APlateSpawner::SetPlate(class APlate* Plate)
 {
+	if (nullptr != CookingPtr)
+	{
+		PlateMap.Add(PlateNum, CookingPtr);
+		for (int i = 0; i < PlateNum; i++)
+		{
+			PlateMap[i]->AddActorLocalOffset(FVector::UnitZ() * 30.0f);
+		}
+		CookingPtr = nullptr;
+		PlateNum++;
+	}
+
 	CookingPtr = Cast<APlate>(Plate);
 	CookingPtr->SetCookingTable_Implementation(this);
 
 	//Cooking을 Attach 시킬것
 	CookingPtr->AttachToComponent(ComponentForCooking, FAttachmentTransformRules::KeepRelativeTransform);
 	CookingPtr->SetActorLocation(ComponentForCooking->GetComponentLocation());
+}
+
+ACooking* APlateSpawner::Interact(AActor* ChefActor)
+{
+	if (nullptr == CookingPtr && PlateNum > 0)
+	{
+		CookingPtr = PlateMap[PlateNum - 1];
+		PlateMap.Remove(PlateNum - 1);
+		PlateNum--;
+	}
+	ACooking* CookingReturn = CookingPtr;
+	CookingPtr = nullptr;
+	return CookingReturn;
 }
 
 void APlateSpawner::PlaceItem(ACooking* ReceivedCooking)
