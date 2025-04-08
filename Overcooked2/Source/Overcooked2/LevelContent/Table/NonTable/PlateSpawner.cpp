ac2b0eefd9931b85289c2a82db9a322ae94e281e
dfgh2727 <dfgh2727@naver.com>
2025-04-08 12:04:57 +0900

    이수환 : PlateSpawner 구현

--- Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
+++ Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
@@ -21,31 +21,25 @@ void APlateSpawner::Tick(float DeltaTime)
 
 void APlateSpawner::SetPlate(class APlate* Plate)
 {
-	if (nullptr != CookingPtr)
-	{
-		PlateMap.Add(PlateNum, CookingPtr);
-		for (int i = 0; i < PlateNum; i++)
-		{
-			PlateMap[i]->AddActorLocalOffset(FVector::UnitZ() * 30.0f);
-		}
-		CookingPtr = nullptr;
-		PlateNum++;
-	}
-
 	CookingPtr = Cast<APlate>(Plate);
 	CookingPtr->SetCookingTable_Implementation(this);
 
-	//Cooking을 Attach 시킬것
-	CookingPtr->AttachToComponent(ComponentForCooking, FAttachmentTransformRules::KeepRelativeTransform);
-	CookingPtr->SetActorLocation(ComponentForCooking->GetComponentLocation());
+	PlateNum++;
+	PlateMap.Add(PlateNum, CookingPtr);
+
+	PlateMap[PlateNum]->AttachToComponent(ComponentForCooking, FAttachmentTransformRules::KeepRelativeTransform);
+	PlateMap[PlateNum]->SetActorLocation(ComponentForCooking->GetComponentLocation());
+	PlateMap[PlateNum]->AddActorLocalOffset(FVector::UnitZ() * 30.0f * (PlateNum -1));
+
+	CookingPtr = nullptr;
 }
 
 ACooking* APlateSpawner::Interact(AActor* ChefActor)
 {
-	if (nullptr == CookingPtr && PlateNum > 0)
+	if (PlateNum > 0)
 	{
-		CookingPtr = PlateMap[PlateNum - 1];
-		PlateMap.Remove(PlateNum - 1);
+		CookingPtr = PlateMap[PlateNum];
+		PlateMap.Remove(PlateNum);
 		PlateNum--;
 	}
 	ACooking* CookingReturn = CookingPtr;
