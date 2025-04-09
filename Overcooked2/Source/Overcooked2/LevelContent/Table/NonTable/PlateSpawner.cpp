e4bfbe1b973d917285d62e0303481067085414d0
성민 류 <baekhun0330@naver.com>
2025-04-09 15:23:27 +0900

    류성민 : Plate Spawner 동기화 테스트

--- Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
+++ Overcooked2/Source/Overcooked2/LevelContent/Table/NonTable/PlateSpawner.cpp
@@ -24,27 +24,59 @@ void APlateSpawner::SetPlate(class APlate* Plate)
 	CookingPtr = Cast<APlate>(Plate);
 	CookingPtr->SetCookingTable_Implementation(this);
 
-	PlateNum++;
-	PlateMap.Add(PlateNum, CookingPtr);
+	if (nullptr == Plate)
+	{
+		return;
+	}
 
-	PlateMap[PlateNum]->AttachToComponent(ComponentForCooking, FAttachmentTransformRules::KeepRelativeTransform);
-	PlateMap[PlateNum]->SetActorLocation(ComponentForCooking->GetComponentLocation());
-	PlateMap[PlateNum]->AddActorLocalOffset(FVector::UnitZ() * 30.0f * (PlateNum -1));
+	if (nullptr == OwnedPlate) // 
+	{
+		OwnedPlate = Plate;
+		OwnedPlate->AttachToComponent(ComponentForCooking, FAttachmentTransformRules::KeepRelativeTransform);
+		OwnedPlate->SetActorLocation(ComponentForCooking->GetComponentLocation());
+		OwnedPlate->AddActorLocalOffset(FVector(0, 0, 0));
+	}
+	else
+	{
+		OwnedPlate->StackPlate(Plate);
+		if (1 == OwnedPlate->GetAnotherPlatesRef().Num())
+		{
+			OwnedPlate->AddActorLocalOffset(FVector(0, 0, 10));
+		}
+	}
+	//PlateNum++;
+	//PlateMap.Add(PlateNum, CookingPtr);
+	//PlateMap[PlateNum]->SetActorLocation(ComponentForCooking->GetComponentLocation());
+	//PlateMap[PlateNum]->AddActorLocalOffset(FVector::UnitZ() * 30.0f * (PlateNum -1));
 
 	CookingPtr = nullptr;
 }
 
 ACooking* APlateSpawner::Interact(AActor* ChefActor)
 {
-	if (PlateNum > 0)
+	if (nullptr != OwnedPlate) // 내가 접시를 스폰한게 있으면
 	{
-		CookingPtr = PlateMap[PlateNum];
-		PlateMap.Remove(PlateNum);
-		PlateNum--;
+		ACooking* NewCooking = Cast<ACooking>(OwnedPlate);
+		if (nullptr != NewCooking)
+		{
+			// 접시를 캐릭터에게 넘겨주고 나는 가지고 있는 접시가 없다.
+			OwnedPlate = nullptr; 
+			return NewCooking;
+		}
 	}
-	ACooking* CookingReturn = CookingPtr;
-	CookingPtr = nullptr;
-	return CookingReturn;
+
+	// 내가 가지고 있는 접시가 없다면 널 반환
+	return nullptr;
+
+	//if (PlateNum > 0)
+	//{
+	//	CookingPtr = PlateMap[PlateNum];
+	//	PlateMap.Remove(PlateNum);
+	//	PlateNum--;
+	//}
+	//ACooking* CookingReturn = CookingPtr;
+	//CookingPtr = nullptr;
+	//return CookingReturn;
 }
 
 void APlateSpawner::PlaceItem(ACooking* ReceivedCooking)
