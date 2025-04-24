dbd83310ab297abb87832c198f26dc8bf524c24d
dfgh2727 <dfgh2727@naver.com>
2025-04-24 11:24:30 +0900

    이수환 : Prawn 위치 조정

--- Overcooked2/Source/Overcooked2/LevelContent/Table/CookingTable.cpp
+++ Overcooked2/Source/Overcooked2/LevelContent/Table/CookingTable.cpp
@@ -65,8 +65,8 @@ void ACookingTable::PlaceItem(ACooking* ReceivedCooking)
 
 		if (EIngredientType::EIT_PRAWN == TempIngredient->GetIngredientType() && EIngredientState::EIS_CHOPPED ==TempIngredient->GetCurIngredientState())
 		{
-			CookingPtr->AddActorWorldOffset(FVector::UnitZ() * (-40.0f));
-			CookingPtr->AddActorWorldRotation(FRotator(135.0f, 0.0f, 0.0f));
+			CookingPtr->AddActorWorldOffset(FVector::UnitX() * 15.0f + FVector::UnitY() * (-20.0f) + FVector::UnitZ() * (-50.0f));
+			CookingPtr->AddActorWorldRotation(FRotator(0.0f, 90.0f, 135.0f));
 		}
 	}
 
